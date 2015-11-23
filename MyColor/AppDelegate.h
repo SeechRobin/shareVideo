1ea88f8d9c217d233ae45029f95a8aeb080d0c92
Robin Mukanganise <seechrobin@gmail.com>
2015-11-23 09:22:39 +0200

    Initial Commit

---
+++ MyColor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyColor
+//
+//  Created by Robin Mukanganise on 2015/11/23.
+//  Copyright © 2015 Robin Mukanganise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
